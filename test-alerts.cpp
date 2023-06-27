e6c59749169157f7659a25be30d6731495498915
ggowtham216 <g.gowtham216@gmail.com>
2023-06-27 22:25:53 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,14 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach as TOO_LOW according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("infers the breach as NORMAL according to limits") {
+  REQUIRE(inferBreach(21, 20, 30) == NORMAL);
+}
+
+TEST_CASE("infers the breach as TOO_HIGH according to limits") {
+  REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
 }

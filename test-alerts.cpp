f5d18bc8d8e84c5e171d1e1e78761e57f008d69e
ggowtham216 <g.gowtham216@gmail.com>
2023-06-27 22:47:48 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -11,14 +11,28 @@ TEST_CASE("infers the breach as NORMAL according to limits") {
   REQUIRE(inferBreach(21, 20, 30) == NORMAL);
 }
 
-TEST_CASE("infers the breach as NORMAL when any limit is equal to temparature") {
-  REQUIRE(inferBreach(30, 20, 20) == NORMAL);
+TEST_CASE("infers the breach as NORMAL when temparature, lower limit and upper limit are equal") {
+  REQUIRE(inferBreach(20, 20, 20) == NORMAL);
 }
 
 TEST_CASE("infers the breach as TOO_HIGH according to limits") {
   REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
 }
 
+TEST_CASE("classify the breach type when PASSIVE_COOLING and temparature is -5") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5) == TOO_LOW);
+}
+
 TEST_CASE("classify the breach type when PASSIVE_COOLING and temparature is zero") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 0) == NORMAL);
 }
+
+TEST_CASE("classify the breach type when PASSIVE_COOLING and temparature is 10") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 10) == NORMAL);
+}
+
+TEST_CASE("classify the breach type when PASSIVE_COOLING and temparature is 36") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
+}
+
+
